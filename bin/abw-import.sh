099b048b56b16feb65746e85765045188e0d6c8c
Fabio Lima <fabiolimace@gmail.com>
2024-03-13 17:44:43 -0300

    Add database import
    TODO: implement method `database_import_data`

--- bin/abw-import.sh
+++ bin/abw-import.sh
@@ -8,7 +8,61 @@
 #
 
 OPTSTRING="ic:rvh"
-source ./abw-common.sh
+source `dirname $0`/abw-common.sh
+
+
+function database_create {
+
+    local COLLECTION=${1}
+    local DATABASE=`database $COLLECTION`
+    
+    if [ -f "$DATABASE" ];
+    then
+        return;
+    fi;
+    
+    sqlite3 "$DATABASE" <<EOF
+CREATE TABLE text_ (uuid_ TEXT PRIMARY KEY, content_ TEXT);
+CREATE TABLE data_ (uuid_ TEXT PRIMARY KEY, token_ TEXT, type_ TEXT, count_ INTEGER, ratio_ REAL, format_ TEXT, case_ TEXT, length_ INTEGER, indexes_ TEXT);
+CREATE TABLE meta_ (uuid_ TEXT PRIMARY KEY, hash_ TEXT, name_ TEXT, path_ TEXT, mime_ TEXT, date_ INTEGER, lines_ INTEGER, words_ INTEGER, bytes_ INTEGER, chars_ INTEGER);
+CREATE UNIQUE INDEX text_index_ on text_ (uuid_);
+CREATE UNIQUE INDEX data_index_ on data_ (uuid_);
+CREATE UNIQUE INDEX meta_index_ on meta_ (uuid_);
+EOF
+
+}
+
+function database_import_text {
+    local COLLECTION="${1}"
+    local TEXT="${2}"
+    local UUID="${3}"
+    local CONTENT=`cat "$TEXT"`
+    local DATABASE=`database $COLLECTION`
+
+    sqlite3 "$DATABASE" "INSERT INTO text_ (uuid_, content_) values ('$UUID', '$CONTENT');";
+}
+
+function database_import_meta {
+    local COLLECTION="${1}"
+    local META="${2}"
+    local UUID="${3}"
+    local HASH="`meta_value "$META" 'hash'`"
+    local NAME="`meta_value "$META" 'name'`"
+    local LANE="`meta_value "$META" 'path'`"
+    local MIME="`meta_value "$META" 'mime'`"
+    local DATE="`meta_value "$META" 'date'`"
+    local LINES="`meta_value "$META" 'lines'`"
+    local WORDS="`meta_value "$META" 'words'`"
+    local BYTES="`meta_value "$META" 'bytes'`"
+    local CHARS="`meta_value "$META" 'chars'`"
+    local DATABASE=`database $COLLECTION`
+    
+    sqlite3 "$DATABASE" "INSERT INTO meta_ (uuid_, hash_, name_, path_, mime_, date_, lines_, words_, bytes_, chars_) values ('$UUID', '$HASH', '$NAME', '$LANE', '$MIME', '$DATE', '$LINES', '$WORDS', '$BYTES', '$CHARS');";
+}
+
+function database_import_data {
+    echo # TODO
+}
 
 function import_file {
 
@@ -33,6 +87,7 @@ function import_file {
     local TEXT=$ROAD/text.txt
     local META=$ROAD/meta.txt
     local DATA=$ROAD/data.tsv
+    local SQLI=
     
     if [[ -d $ROAD ]];
     then
@@ -41,18 +96,21 @@ function import_file {
     fi;
     
     mkdir --parents $ROAD;
+    database_create "$COLLECTION"
     
     cp $INPUT_FILE $TEXT
+    database_import_text $COLLECTION $TEXT $UUID
     
     echo -n > $META
     echo "collection=$COLLECTION" >> $META
     echo "uuid=$UUID" >> $META
     echo "hash=$HASH" >> $META
-    echo "path=`realpath $INPUT_FILE`" >> $META
     echo "name=`basename $INPUT_FILE`" >> $META
-    echo "date=`date '+%F %T'`" >> $META
+    echo "path=`realpath $INPUT_FILE`" >> $META
     echo "mime=`file -bi $INPUT_FILE`" >> $META
+    echo "date=`date '+%F %T'`" >> $META
     wc -lwcm "$INPUT_FILE" | awk '{ printf "lines=%d\nwords=%d\nbytes=%d\nchars=%d\n", $1, $2, $3, $4; }' >> $META
+    database_import_meta $COLLECTION $META $UUID
 
     $BASEDIR/abw-process.sh $TEXT > $DATA
     
