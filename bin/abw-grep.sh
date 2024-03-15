c9e65ed178f4c3dee119bad259fb8267201d559c
Fabio Lima <fabiolimace@gmail.com>
2024-03-15 03:16:48 -0300

    Update abw-grep to search rows from the SQLite database using GLOB

--- bin/abw-grep.sh
+++ bin/abw-grep.sh
@@ -4,26 +4,45 @@
 #
 #    abw-grep REGEX
 #    abw-grep -c COLLECTION REGEX
-#    abw-grep -c COLLECTION -m REGEX
-#    abw-grep -c COLLECTION -d REGEX
+#
+# Note: it it uses GLOB when the collection is a SQLite database.
 #
 
-OPTSTRING="gc:md"
+OPTSTRING="gc:"
 source `dirname $0`/abw-common.sh
+
+function grep_fs {
+
+    local COLLECTION="${1}"
+    local REGEX="${2}"
+
+    grep -E -r --color=auto "$REGEX" "$DATADIR/$COLLECTION"/*/*/*/text.txt
+}
+
+function glob_db {
+
+    local COLLECTION="${1}"
+    local REGEX="${2}"
+    
+    local DATABASE=`database $COLLECTION`
+    
+    sqlite3 "$DATABASE" "select * from text_ where content_ glob '$REGEX'";
+}
 
 REGEX="${1}";
 COLLECTION="${options["c"]:-default}"
 
-if [[ ! -d "$DATADIR/$COLLECTION" ]]; then
-    echo "abw-grep.sh: $COLLECTION: Collection not found" 1>&2;
+if [[ -z "$REGEX" ]]; then
+    echo "abw-show.sh: REGEX required" 1>&2;
     exit 1;
 fi;
 
-if [[ ${options["m"]} ]]; then
-    grep -E -r --color=auto "$REGEX" "$DATADIR/$COLLECTION"/*/*/*/meta.txt
-elif [[ ${options["d"]} ]]; then
-    grep -E -r --color=auto "$REGEX" "$DATADIR/$COLLECTION"/*/*/*/data.tsv
+if [[ -d "$DATADIR/$COLLECTION" ]]; then
+    grep_fs "$COLLECTION" "$REGEX";
+elif [[ -f "$DATADIR/$COLLECTION.db" ]]; then
+    glob_db "$COLLECTION" "$REGEX";
 else
-    grep -E -r --color=auto "$REGEX" "$DATADIR/$COLLECTION"/*/*/*/text.txt
+    echo "abw-grep.sh: $COLLECTION: Collection not found" 1>&2;
+    exit 1;
 fi;
 
