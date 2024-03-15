3b7e67baa8cd2ad76db92fce7a7687f765df5744
Fabio Lima <fabiolimace@gmail.com>
2024-03-15 03:16:06 -0300

    Update abw-show to show rows from the SQLite database

--- bin/abw-show.sh
+++ bin/abw-show.sh
@@ -11,30 +11,57 @@
 OPTSTRING="sc:md"
 source `dirname $0`/abw-common.sh
 
+function show_fs {
+
+    local COLLECTION="${1}"
+    local UUID="${2}"
+
+    DIRECTORY=`directory "$COLLECTION" "$UUID"`;
+
+    if [[ ! -d "$DIRECTORY" ]]; then
+        return;
+    fi;
+
+    if [[ ${options["m"]} ]]; then
+        cat "$DIRECTORY/meta.txt"
+    elif [[ ${options["d"]} ]]; then
+        cat "$DIRECTORY/data.txt"
+    else
+        cat "$DIRECTORY/text.txt"
+    fi;
+}
+
+# TODO
+function show_db {
+
+    local COLLECTION="${1}"
+    local UUID="${2}"
+
+    local DATABASE=`database $COLLECTION`
+    
+    if [[ ${options["m"]} ]]; then
+        sqlite3 "$DATABASE" "select * from meta_ where uuid_ = '$UUID'";
+    elif [[ ${options["d"]} ]]; then
+        sqlite3 "$DATABASE" "select * from data_ where uuid_ = '$UUID'";
+    else
+        sqlite3 "$DATABASE" "select * from text_ where uuid_ = '$UUID'";
+    fi;
+}
+
 UUID="${1}";
 COLLECTION="${options["c"]:-default}"
-ROAD=`directory "$COLLECTION" "$UUID"`;
 
 if [[ -z "$UUID" ]]; then
     echo "abw-show.sh: UUID required" 1>&2;
     exit 1;
 fi;
 
-if [[ ! -d "$DATADIR/$COLLECTION" ]]; then
-    echo "abw-show.sh: $COLLECTION: Collection not found" 1>&2;
-    exit 1;
-fi;
-
-if [[ ! -d "$ROAD" ]]; then
-    echo "abw-show.sh: $UUID: UUID not found" 1>&2;
-    exit 1;
-fi;
-
-if [[ ${options["m"]} ]]; then
-    cat "$ROAD/meta.txt"
-elif [[ ${options["d"]} ]]; then
-    cat "$ROAD/data.tsv"
+if [[ -d "$DATADIR/$COLLECTION" ]]; then
+    show_fs "$COLLECTION" "$UUID";
+elif [[ -f "$DATADIR/$COLLECTION.db" ]]; then
+    show_db "$COLLECTION" "$UUID";
 else
-    cat "$ROAD/text.txt"
+    echo "abw-grep.sh: $COLLECTION: Collection not found" 1>&2;
+    exit 1;
 fi;
 
